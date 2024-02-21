ece73b04ea32ca9dc1014d1f82a2bc166ad404a6
sandrodutra <sandrodutra@cwi.com.br>
2024-02-21 16:21:49 -0300

    Adicionado configuração do plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,23 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // ..
+      'publisher': {
+          enabled: true,
+          config: {
+              hooks: {
+                  beforePublish: async ({ strapi, uid, entity }) => {
+                      console.log('beforePublish');
+                  },
+                  afterPublish: async ({ strapi, uid, entity }) => {
+                      console.log('afterPublish');
+                  },
+                  beforeUnpublish: async ({ strapi, uid, entity }) => {
+                      console.log('beforeUnpublish');
+                  },
+                  afterUnpublish: async ({ strapi, uid, entity }) => {
+                      console.log('afterUnpublish');
+                  },
+              },
+          },
+      },
+      // ..
+  });
